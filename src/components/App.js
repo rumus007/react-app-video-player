09ca97d57fd010476306aeebd1e2f3f0347dc45e
Rumus Bajracharya <rumus.3353@gmail.com>
2022-02-25 10:41:51 +0545

    Added youtube api fetch functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import youtube from "../repositories/youtube";
 import SearchBar from "./SearchBar/SearchBar";
 
 class App extends React.Component {
+  onSubmit = async (term) => {
+    console.log("from app>>", term);
+    let results = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    console.log("results are>>>", results);
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onSubmit={this.onSubmit} />
       </div>
     );
   }
